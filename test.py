df723cc2cd7a05dbc5b895bcab6b1d5f985f8476
Jonathan Henrich <jonathan.henrich@stud.uni-goettingen.de>
2022-01-13 16:58:31 +0100

    commit

--- test.py
+++ test.py
@@ -1,27 +1,8 @@
 import torch
-import torchvision
 import os
 import numpy as np
-import json
-import pandas as pd
-
-# paths
-root_dir = "G:/.shortcut-targets-by-id/1j3VRcg8GosML98qgJcN3di-lgTFtcd0n/data/"
-hostpics_path = root_dir + "hostpics"
-
-# missing_data_path = root_dir + "missing_data.json"
-# listings_meta_path = root_dir + "data1/listings.csv.gz"
-# listings_path = root_dir + "/data1/listings.csv"
-
-# data
-# listings_meta = pd.read_csv(listings_meta_path)
-# listings = pd.read_csv(listings_path)
-#
-# with open(missing_data_path, "r") as temp:
-#     missing_data = json.load(temp)
 
 
-# dataset
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, filepath, ndata):
         self.filepath = filepath
@@ -38,24 +19,51 @@ class Dataset(torch.utils.data.Dataset):
         return x
 
 
-dataset = Dataset(hostpics_path, 10)
-dataloader = torch.utils.data.DataLoader(dataset, bytch_size=1, shuffle=False)
+def compute_train_features(device, dataloader, feature_extractor, output_dim=[512, 16, 16]):
+    feature_extractor.to(device)
+    feature_extractor.eval()
 
-vgg = torchvision.models.vgg19(pretrained=True)
-eature_extractor = vgg.features[0:31]
+    ndata = len(dataloader.dataset)
+    output = torch.empty(ndata, output_dim[0], output_dim[1], output_dim[2])
+    start, stop = 0, 0
+
+    with torch.no_grad:
+        for batch in dataloader:
+            stop += len(batch)
+            output[start:stop, ...] = feature_extractor(batch.to(device))
+            start += len(batch)
+
+    return output
 
 
+if __name__ == "main":
+    import torchvision
+    import json
+    import pandas as pd
 
+    # paths
+    root_dir = "G:/.shortcut-targets-by-id/1j3VRcg8GosML98qgJcN3di-lgTFtcd0n/data/"
+    hostpics_path = root_dir + "hostpics"
 
-# def compute_train_features(dataloader):
-#     feature_extractor.to(device)
-#     feature_extractor.eval()
-#     out = torch.empty(501, 512, 16, 16)
-#     start, stop = 0, 0
-#
-#     for batch in dataloader:
-#         stop += len(batch["image"])
-#         out[start:stop, ...] = feature_extractor(batch["image"].to(device)).detach()
-#         start += len(batch["image"])
-#
-#     return out
+    # missing_data_path = root_dir + "missing_data.json"
+    # listings_meta_path = root_dir + "data1/listings.csv.gz"
+    # listings_path = root_dir + "/data1/listings.csv"
+
+    # data
+    # listings_meta = pd.read_csv(listings_meta_path)
+    # listings = pd.read_csv(listings_path)
+    #
+    # with open(missing_data_path, "r") as temp:
+    #     missing_data = json.load(temp)
+
+    # initialize dataset and dataloader
+    dataset = Dataset(hostpics_path, 10)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False)
+
+    # get pretrained model
+    vgg = torchvision.models.vgg19(pretrained=True)
+    feature_extractor = vgg.features[0:31]
+
+    # compute features for later training
+    device = "cpu"
+    compute_train_features(device, dataloader, feature_extractor)
