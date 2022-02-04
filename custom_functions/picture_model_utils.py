07dadb811c6f39087b604ba01ebdbc1cd5ba861d
Jonathan Henrich <jonathan.henrich@stud.uni-goettingen.de>
2022-02-04 15:33:30 +0100

    sadf

--- custom_functions/picture_model_utils.py
+++ custom_functions/picture_model_utils.py
@@ -1,33 +1,29 @@
 import torch
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, picture_dir, response_dir, ndata):
-        self.ndata = ndata
+class ThumbnailsDataset(torch.utils.data.Dataset):
+    def __init__(self, picture_dir, response_dir, split):
 
+        self.split = ["thumbnail" + index + ".npy" for index in split]
         self.picture_dir = picture_dir
         self.response_dir = response_dir
 
-        self.picture_names = os.listdir(self.picture_dir)
-        self.picture_names = self.picture_names[0:ndata]
-
     def __len__(self):
-        return len(self.picture_names)
+        return len(self.split)
 
     def __getitem__(self, key):
-        x = np.load(self.picture_dir + "/" + self.picture_names[key])
+        x = np.load(self.picture_dir + "/" + self.split[key])
 
-        # transform (to tensor and normalize)
+        # transform (to FloatTensor and normalize)
         x = torch.from_numpy(x).type(torch.FloatTensor)
         x /= 255
         x -= torch.tensor([0.485, 0.456, 0.406])
         x /= torch.tensor([0.229, 0.224, 0.225])
         x = torch.permute(x, dims=[2, 0, 1])
 
-        y = np.load(self.response_dir + "/" + self.picture_names[key])
+        y = np.load(self.response_dir + "/" + self.split[key])
         y = torch.from_numpy(y).type(torch.FloatTensor)
         y = torch.log(y)
 
