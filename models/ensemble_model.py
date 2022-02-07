d479a4335d7328a3f2d791342fd2993a31b131f8
dernameistegal <tester.97test@gmail.com>
2022-02-07 17:04:36 +0100

    ensemble

--- models/ensemble_model.py
+++ models/ensemble_model.py
@@ -4,13 +4,10 @@ import torch.nn.functional as F
 
 
 class EnsembleModel(nn.Module):
-    def __init__(self, picture_nn, description_nn, reviews_nn):
+    def __init__(self, nfeatures):
         super(EnsembleModel, self).__init__()
-        self.picture_nn = picture_nn
-        self.description_nn = description_nn
-        self.reviews_nn = reviews_nn
 
-        self.linear1 = nn.Linear(330, 100)
+        self.linear1 = nn.Linear(nfeatures, 100)
         self.linear2 = nn.Linear(100, 100)
         self.linear3 = nn.Linear(100, 1)
 
