3edb11e0536bc33f019741b2d1f087cfd1397fcb
Jonathan Henrich <jonathan.henrich@stud.uni-goettingen.de>
2022-02-09 14:05:17 +0100

    sdf
    sdf

--- models/ensemble_model.py
+++ models/ensemble_model.py
@@ -7,11 +7,10 @@ class EnsembleModel(nn.Module):
     def __init__(self, nfeatures):
         super(EnsembleModel, self).__init__()
 
-        self.linear1 = nn.Linear(300 + nfeatures, 100)
-        self.linear2 = nn.Linear(100, 100)
-        self.linear3 = nn.Linear(100, 100)
-        self.linear4 = nn.Linear(100, 100)
-        self.linear5 = nn.Linear(100, 1)
+        self.linear1 = nn.Linear(300 + nfeatures, 300)
+        self.linear2 = nn.Linear(300, 200)
+        self.linear3 = nn.Linear(200, 100)
+        self.linear4 = nn.Linear(100, 1)
 
     def forward(self, pic_embedding, description_embedding, reviews_embedding, features):
         embeddings = torch.hstack((pic_embedding, description_embedding, reviews_embedding, features))
@@ -20,6 +19,5 @@ class EnsembleModel(nn.Module):
         x = self.linear2(F.relu(x))
         x = self.linear3(F.relu(x))
         x = self.linear4(F.relu(x))
-        x = self.linear5(F.relu(x))
 
         return x
