d993067718ac4c9050fb91d3d9b4e77d0fd6efe1
Jonathan Henrich <jonathan.henrich@stud.uni-goettingen.de>
2022-02-09 14:19:09 +0100

    df

--- models/ensemble_model.py
+++ models/ensemble_model.py
@@ -7,17 +7,15 @@ class EnsembleModel(nn.Module):
     def __init__(self, nfeatures):
         super(EnsembleModel, self).__init__()
 
-        self.linear1 = nn.Linear(300 + nfeatures, 300)
-        self.linear2 = nn.Linear(300, 200)
-        self.linear3 = nn.Linear(200, 100)
-        self.linear4 = nn.Linear(100, 1)
+        self.linear1 = nn.Linear(300 + nfeatures, 500)
+        self.linear2 = nn.Linear(500, 500)
+        self.linear3 = nn.Linear(500, 1)
 
     def forward(self, pic_embedding, description_embedding, reviews_embedding, features):
         embeddings = torch.hstack((pic_embedding, description_embedding, reviews_embedding, features))
 
         x = self.linear1(embeddings)
         x = self.linear2(F.relu(x))
-        x = self.linear3(F.relu(x))
-        x = self.linear4(x)
+        x = self.linear3(x)
 
         return x
