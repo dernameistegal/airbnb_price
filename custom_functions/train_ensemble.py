425d0903aa12bcdf591cab9ce70e538ec96a6061
Jonathan Henrich <jonathan.henrich@stud.uni-goettingen.de>
2022-02-09 23:16:40 +0100

    new ensemble model, dataset and train function (not complete)

--- custom_functions/train_ensemble.py
+++ custom_functions/train_ensemble.py
@@ -97,3 +97,106 @@ def run_training(model, optimizer, loss_function, device, num_epochs,
     time_elapsed = np.round(time.time() - start_time, 0).astype(int)
     print(f'Finished training after {time_elapsed} seconds.')
     return train_losses, val_losses, train_rmse, val_rmse
+
+
+
+
+############################################################################
+##########################run training with embeddings
+############################################################################
+#self.thumb_X[idx], self.desc_X[idx], self.rev_X[idx], self.cont_X[idx], self.cat_X[idx], self.output[idx]
+#def forward(self, thumb_data, desc_data, rev_data, cont_data, cat_data):
+
+def train(dataloader, optimizer, model, loss_fn, device, ntrain, master_bar):
+    """Run one training epoch."""
+
+    epoch_loss = []
+    i = 0
+
+    for pic_embdg, description_embdg, reviews_embdg, cont_features, cat_features, label in fastprogress.progress_bar(dataloader, parent=master_bar):
+        pic_embdg, description_embdg, reviews_embdg, cont_features, cat_features, label = \
+            pic_embdg.to(device), description_embdg.to(device), reviews_embdg.to(device), \
+            cont_features.to(device), cat_features.to(device), label.to(device)
+
+        optimizer.zero_grad()
+        model.train()
+
+        # Forward pass
+        with torch.cuda.amp.autocast():
+            labels_pred = model(pic_embdg, description_embdg, reviews_embdg, cont_features, cat_features)
+            labels_pred = torch.squeeze(labels_pred)
+            loss = loss_fn(labels_pred, label)
+
+        # Backward pass
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # For plotting the train loss, save it for each sample
+        epoch_loss.append(loss.item())
+        i = i + 1
+        if i > ntrain:
+            break
+
+    # Return the mean loss and the accuracy of this epoch
+    return np.mean(epoch_loss), np.sqrt(np.mean(epoch_loss))
+
+
+def validate(dataloader, model, loss_fn, device, nval, master_bar):
+    epoch_loss = []
+
+    model.eval()
+    i = 0
+    with torch.no_grad():
+        for pic_embdg, description_embdg, reviews_embdg, features, label in fastprogress.progress_bar(dataloader, parent=master_bar):
+            pic_embdg, description_embdg, reviews_embdg, features, label = \
+                pic_embdg.to(device).float(), description_embdg.to(device).float(), reviews_embdg.to(device).float(), \
+                features.to(device).float(), label.to(device).float()
+
+            # make a prediction on validation set
+            labels_pred = model(pic_embdg, description_embdg, reviews_embdg, features)
+            labels_pred = torch.squeeze(labels_pred)
+
+            # Compute loss
+            loss = loss_fn(labels_pred, label)
+
+            # For plotting the train loss, save it for each sample
+            epoch_loss.append(loss.item())
+            i = i + 1
+            if i > nval:
+                break
+
+    return np.mean(epoch_loss), np.sqrt(np.mean(epoch_loss))
+
+
+def run_training(model, optimizer, loss_function, device, num_epochs,
+                 train_dataloader, val_dataloader, ntrain=150, nval=50, verbose=False, scheduler=None):
+    start_time = time.time()
+    master_bar = fastprogress.master_bar(range(num_epochs))
+    train_losses, val_losses, train_rmse, val_rmse = [], [], [], []
+    for epoch in master_bar:
+        # Train the model
+        epoch_train_loss, epoch_train_acc = train(train_dataloader, optimizer, model,
+                                                  loss_function, device, ntrain, master_bar)
+        # Validate the model
+        epoch_val_loss, epoch_val_acc = validate(val_dataloader, model, loss_function, device, nval, master_bar)
+
+        # Save loss and acc for plotting and add increase of val_acc
+        train_losses.append(epoch_train_loss)
+        val_losses.append(epoch_val_loss)
+        train_rmse.append(epoch_train_acc)
+        val_rmse.append(epoch_val_acc)
+        if val_rmse[-1] <= np.min(val_rmse):
+            state = model.state_dict()
+            print("Saving model...")
+            savepath = "/content/checkpoints/checkpoint.pt"
+            torch.save(state, savepath)
+        if scheduler:
+            scheduler.step()
+        if verbose:
+            master_bar.write(
+                f'Train loss: {epoch_train_loss:.2f}, val loss: {epoch_val_loss:.2f}, train rmse: {epoch_train_acc:.3f}, val rmse {epoch_val_acc:.3f}')
+
+    time_elapsed = np.round(time.time() - start_time, 0).astype(int)
+    print(f'Finished training after {time_elapsed} seconds.')
+    return train_losses, val_losses, train_rmse, val_rmse
--- models/ensemble_model.py
+++ models/ensemble_model.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import Dataset
+import numpy as np
 
 
 class EnsembleModel(nn.Module):
@@ -19,3 +21,113 @@ class EnsembleModel(nn.Module):
         x = self.linear3(x)
 
         return x
+
+
+# requires label encoding for categories
+class EnsembleModel2(nn.Module):
+
+    def __init__(self, no_of_thumb, no_of_desc, no_of_rev, no_of_cont, cat_emb_dims, lin_layer_sizes):
+        super().__init__()
+
+        # number of features for different feature types
+        self.no_of_thumb = no_of_thumb
+        self.no_of_desc = no_of_desc
+        self.no_of_rev = no_of_rev
+        self.no_of_cont = no_of_cont
+        self.no_of_cat = np.sum([y for x, y in cat_emb_dims])
+
+        # Embedding layers
+        self.cat_emb_layers = nn.ModuleList([nn.Embedding(x, y)
+                                             for x, y in cat_emb_dims])
+        # Linear Layers
+        first_lin_layer = nn.Linear(
+            self.no_of_thumb + self.no_of_desc + self.no_of_rev + self.no_of_cont + self.no_of_cat,
+            lin_layer_sizes[0])
+
+        self.lin_layers = nn.ModuleList([first_lin_layer] +
+                                        [nn.Linear(lin_layer_sizes[i], lin_layer_sizes[i + 1])
+                                         for i in range(len(lin_layer_sizes) - 1)])
+
+        for lin_layer in self.lin_layers:
+            nn.init.kaiming_normal_(lin_layer.weight.data)
+
+        self.last_lin_layer = nn.Linear(lin_layer_sizes[-1], 1)
+        nn.init.kaiming_normal_(self.last_lin_layer.weight.data)
+
+        # Batch Norm Layers
+        self.first_bn_layer = nn.BatchNorm1d(self.no_of_thumb + self.no_of_desc + self.no_of_rev, self.no_of_cont)
+        self.bn_layers = nn.ModuleList([nn.BatchNorm1d(size)
+                                        for size in lin_layer_sizes])
+
+        # Dropout Layers (can be modified by using dropout rate as argument)
+        self.thumb_dropout = nn.Dropout(0.5)
+        self.desc_dropout = nn.Dropout(0.5)
+        self.rev_dropout = nn.Dropout(0.5)
+        self.cat_dropout_layer = nn.Dropout(0.5)
+        self.linear_droput_layers = nn.ModuleList([nn.Dropout(0.5)] * len(lin_layer_sizes))
+
+    def forward(self, thumb_data, desc_data, rev_data, cont_data, cat_data):
+
+        # generate embeddings and apply dropout
+        x = [cat_emb_layer(cat_data[:, i])
+             for i, cat_emb_layer in enumerate(self.cat_emb_layers)]
+
+        x = torch.cat(x, 1)
+        x = self.emb_dropout_layer(x)
+
+        # normalize data of other features and apply dropout
+        cont_data = torch.cat([thumb_data, desc_data, rev_data, cont_data], dim=1)
+        normalized_cont_data = self.first_bn_layer(cont_data)
+
+        x = torch.cat([x, normalized_cont_data], dim=1)
+
+        for lin_layer, dropout_layer, bn_layer in \
+                zip(self.lin_layers, self.droput_layers, self.bn_layers):
+            x = F.relu(lin_layer(x))
+            x = bn_layer(x)
+            x = dropout_layer(x)
+
+        x = self.output_layer(x)
+
+        return x
+
+
+class EnsembleDataset2(Dataset):
+    def __init__(self, data, desc_col, rev_col, thumb_col, cat_cols, output_col):
+        self.length = len(data)
+
+        # lists of columns that belong to predictor type (category, continuous, description, ...)
+        self.des_col = desc_col
+        self.rev_col = rev_col
+        self.thumb_col = thumb_col
+        self.cat_cols = cat_cols
+        self.cont_cols = [col for col in data.columns
+                          if col not in self.cat_cols + [self.desc_col, self.rev_col, self.thumb_col, self.output_col]]
+
+        # actual data that belongs to predictors
+        self.desc_X = data[self.des_col].astype(np.float64).values.reshape(-1, 1)
+        self.desc_X = np.apply_along_axis(np.concatenate, 1, self.desc_X)
+        self.desc_X = torch.from_numpy(self.desc_X)
+
+        self.rev_X = data[self.rev_col].astype(np.float64).values.reshape(-1, 1)
+        self.rev_X = np.apply_along_axis(np.concatenate, 1, self.rev_X)
+        self.rev_X = torch.from_numpy(self.rev_X)
+
+        self.thumb_X = data[self.thumb_col].astype(np.float64).values.reshape(-1, 1)
+        self.thumb_X = np.apply_along_axis(np.concatenate, 1, self.thumb_X)
+        self.thumb_X = torch.from_numpy(self.thumb_X)
+
+        self.cont_X = data[self.cont_cols].astype(np.float64).values
+        self.cont_X = torch.from_numpy(self.cont_X)
+
+        self.cat_X = data[self.cat_cols].astype(np.int64).values
+        self.cat_X = torch.from_numpy(self.cat_X)
+
+        self.output = data[output_col].astype(np.float64).values.reshape(-1, 1)
+        self.output = torch.from_numpy(self.output)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        return self.thumb_X[idx], self.desc_X[idx], self.rev_X[idx], self.cont_X[idx], self.cat_X[idx], self.output[idx]
