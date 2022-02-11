ca48f61c5c95166bd7597cb3ab0ef94d50541388
Jonathan Henrich <jonathan.henrich@stud.uni-goettingen.de>
2022-02-11 16:56:33 +0100

    sdf

--- custom_functions/train_ensemble2.py
+++ custom_functions/train_ensemble2.py
@@ -22,8 +22,8 @@ def train(dataloader, optimizer, model, loss_fn, device, ntrain, master_bar):
 
     for pic_embdg, description_embdg, reviews_embdg, cont_features, cat_features, label in fastprogress.progress_bar(dataloader, parent=master_bar):
         pic_embdg, description_embdg, reviews_embdg, cont_features, cat_features, label = \
-            pic_embdg.to(device), description_embdg.to(device), reviews_embdg.to(device), \
-            cont_features.to(device), cat_features.to(device), label.to(device)
+            pic_embdg.to(device).float(), description_embdg.to(device).float(), reviews_embdg.to(device).float(), \
+            cont_features.to(device).float(), cat_features.to(device).float(), label.to(device).float()
 
         optimizer.zero_grad()
         model.train()
@@ -58,9 +58,9 @@ def validate(dataloader, model, loss_fn, device, nval, master_bar):
         for pic_embdg, description_embdg, reviews_embdg, cont_features, cat_features, label in fastprogress.progress_bar(
                 dataloader, parent=master_bar):
             pic_embdg, description_embdg, reviews_embdg, cont_features, cat_features, label = \
-                pic_embdg.to(device), description_embdg.to(device), reviews_embdg.to(device), \
-                cont_features.to(device), cat_features.to(device), label.to(device)
-
+                pic_embdg.to(device).float(), description_embdg.to(device).float(), reviews_embdg.to(device).float(), \
+                cont_features.to(device).float(), cat_features.to(device).float(), label.to(device).float()
+            print(cont_features)
             # make a prediction on validation set
             labels_pred = model(pic_embdg, description_embdg, reviews_embdg, cont_features, cat_features)
             labels_pred = torch.squeeze(labels_pred)
